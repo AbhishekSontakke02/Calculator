dc7da91cfe59c6c6b5c8d5c23498cdaa9b93a225
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-08-02 14:56:03 +0530

    Added: Adiition Function that returns addition of a & b

--- src/GitHub/Calculator.java
+++ src/GitHub/Calculator.java
@@ -1,10 +1,31 @@
 package GitHub;
 
+import java.util.Scanner;
+
 public class Calculator {
+	static Scanner sc = new Scanner(System.in);
+	static int a,b,result;
+	static String oprator;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Calculator!");
-		int a = 20,b = 10, c;
-		c = a + b;
-		System.out.println("Addition of A and B is = "+c);
+		System.out.println("Enter the Value Of A = ");
+		a =  Integer.parseInt(sc.nextLine());
+		System.out.println("Enter the Value Of B = ");
+		b = Integer.parseInt(sc.nextLine());
+		System.out.println("Enter the Opration to perform:");
+		oprator = sc.nextLine().trim().toLowerCase();
+		switch(oprator) {
+		case "add":
+			result = add(a,b);
+			System.out.println(result);
+			break;
+		default :
+			System.out.println("Enter Valid Opration");
+		}
+		
 	}
+	private static int add(int a , int b) {
+		return a+b;
+	}
+	
 }
