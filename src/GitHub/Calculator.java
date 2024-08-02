a22ddb2f0e58c77867898f5f8ef70f8ee8808959
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-08-02 12:18:04 +0530

    Added : Implemented Substratcion functionality

--- src/GitHub/Calculator.java
+++ src/GitHub/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
 		System.out.println("Welcome to Calculator!");
 		int a = 20,b = 10, c;
 		c = a + b;
-		System.out.println("Addition of A and B is = "+c);
+		System.out.println("Addition of A + B  = "+c);
+		c = a-b;
+		System.out.println("Substration of A - B = "+c);
+
 	}
 }
