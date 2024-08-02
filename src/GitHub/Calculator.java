e6343bb7cdb8cffb3262d971a1186deb123958cd
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-08-02 11:33:29 +0530

    Added: Addition Functionlity in calculator

--- src/GitHub/Calculator.java
+++ src/GitHub/Calculator.java
@@ -3,5 +3,8 @@ package GitHub;
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Calculator!");
+		int a = 20,b = 10, c;
+		c = a + b;
+		System.out.println("Addition of A and B is = "+c);
 	}
 }
